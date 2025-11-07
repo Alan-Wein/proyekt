34c221614f26726836b6409cbb44da1ec80651bf
Alan-Wein <alonweintraub.w@gmail.com>
2025-11-07 10:03:54 +0200

    client

--- main.py
+++ main.py
@@ -1,70 +1,61 @@
-import json
-import sqlite3
-conn = sqlite3.connect('users.db')
-c = conn.cursor()
-c.execute("""CREATE TABLE IF NOT EXISTS users (
-            email TEXT,
-            name TEXT,
-            password TEXT,
-            id INTEGER,
-            friends Text
-    )""")
-conn.commit()
+import socket
+
 def start():
+    s = socket.socket()
+    s.connect(("127.0.0.1", 9999))
+
     email = input("Enter email: ")
     name = input("Enter name: ")
     password = input("Enter password: ")
-    c.execute("SELECT COUNT(*) FROM users")
-    id = c.fetchone()[0]
-    lst=[]
     if email == "e" and name == "e" and password == "e":
+        s.send("EXIT".encode())
         return
     if email == "clear" and name == "e" and password == "e":
-        c.execute("DELETE FROM users")
+        s.send("CLEAR".encode())
         start()
-    c.execute("SELECT 1 FROM users WHERE email = ?", (email,))
-    if(c.fetchone() is None):
-
-        c.execute("""INSERT INTO users VALUES (?, ?, ?, ?, ?)""", (email, name, password, id, json.dumps(lst)))
-        print("Hello for the first time!")
-        conn.commit()
-        chat(id)
+        return
     else:
-        c.execute("SELECT 1 FROM users WHERE name = ? AND password = ?" , (name,password))
-        if c.fetchone() is None:
-            print("name or password are incorrect")
-            start()
-        else:
-            c.execute("SELECT id FROM users WHERE name = ?", (name,))
-            id=c.fetchone()[0]
-            print("Welcome back!")
-        conn.commit()
-        chat(id)
+        s.send(f"LOGIN|{email}|{name}|{password}".encode())
+    response = s.recv(2048).decode()
 
-def chat(id):
-    c.execute("SELECT COUNT(*) FROM users")
-    user_len = c.fetchone()[0]
+    if response == "NO":
+        print("Name or password incorrect")
+        exit()
+
+    elif response.startswith("NEW"):
+        id = response.split("|")[1]
+        print("Hello for the first time!")
+
+    elif response.startswith("OK"):
+        id = response.split("|")[1]
+        print("Welcome back!")
+
+    # Chat loop
     while True:
-        c.execute("SELECT * FROM users Where id=?", (id,))
-        user = c.fetchone()
-        lst = json.loads(user[4])
-        text=input("")
-        if text=="list":
-            print("Friends list: " +lst.__str__())
+        text = input("> ")
         if text == "exit":
+            s.send("EXIT".encode())
+            print("Goodbye")
             break
-        if text == "back":
-            start()
-            break
-        if text.startswith("add "):
-            value = text[4:].strip()
-            if value.isdigit() and value != "" and int(value)<user_len and int(value) not in lst and int(value)!=id:
-                lst.append(int(value))
-                c.execute("""UPDATE users SET friends = ? WHERE id = ?""",(json.dumps(lst),id))
-                conn.commit()
-            else:
-                print("Invalid \"add\" ")
+
+        s.send(f"CMD|{id}|{text}".encode())
+        reply = s.recv(2048).decode()
+
+        if reply.startswith("FRIENDS"):
+            print("Friends list:", reply.split("|")[1])
+
+        elif reply.startswith("ME"):
+            print("You are:", reply.split("|")[1])
+
+        elif reply.startswith("ONLINE"):
+            print("Online list:", reply.split("|")[1])
+
+        elif reply == "ADDED":
+            print("Friend added")
+
+        elif reply == "INVALID":
+            print("Invalid command")
 
 
-if __name__ == "__main__":
+if __name__=="__main__":
     start()
