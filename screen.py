4baf16d99331a749183ca02c3a4754ed6b02d355
Alan-Wein <alonweintraub.w@gmail.com>
2025-12-02 11:23:30 +0200

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,7 +1,10 @@
 import tkinter as tk
 root = tk.Tk()
 def button_click(i):
-    textbox.insert(tk.END,f"Friend{i}\n")
+    textbox.insert(tk.END,f"Friend {i}\n")
+
+def _on_mousewheel(event):
+    canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
 
 root.title="program"
 root.grid_columnconfigure(0, weight=1)  # left side expands
@@ -25,6 +28,8 @@ scrollbar.grid(row=2, column=1, sticky="ns")
 canvas.configure(yscrollcommand=scrollbar.set)
 
 scrollable_frame = tk.Frame(canvas)
+scrollable_frame.bind("<Enter>", lambda e: canvas.bind_all("<MouseWheel>", _on_mousewheel))
+scrollable_frame.bind("<Leave>", lambda e: canvas.unbind_all("<MouseWheel>"))
 canvas_window = canvas.create_window((0, 0), window=scrollable_frame, anchor="nw")
 
 # Expand scroll region when size changes
@@ -33,8 +38,10 @@ def update_scroll(event):
 scrollable_frame.bind("<Configure>", update_scroll)
 
 lst=[]
-for i in range(10):
+for i in range(20):
     btn = tk.Button(scrollable_frame, text=f"Butt {i+1}", width=18,command=lambda i=i: button_click(i+1))
+    btn.bind("<Enter>", lambda e: canvas.bind_all("<MouseWheel>", _on_mousewheel))
+    btn.bind("<Leave>", lambda e: canvas.unbind_all("<MouseWheel>"))
     btn.grid(row=i, column=0, padx=5, pady=2, sticky="ew")
 
 scrollable_frame.grid_columnconfigure(0, weight=1)
