c33ff2cf90812ff4802c421d19c8955bc7157e11
Alan-Wein <alonweintraub.w@gmail.com>
2025-12-09 11:39:33 +0200

    Update screen.py

--- screen.py
+++ screen.py
@@ -1,16 +1,32 @@
 import tkinter as tk
-root = tk.Tk()
+import keyboard
 def button_click(i):
+    textbox.configure(state="normal")
     textbox.insert(tk.END,f"Friend {i}\n")
+    textbox.configure(state="disabled")
 
 def _on_mousewheel(event):
-    canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")
+    canvas.yview_scroll(int(-1 * (event.delta / 120)), "units")#how much scroll
+def type():
+    if entrybox.get() != "":
+        textbox.configure(state="normal")
+        textbox.insert(tk.END, ">"+entrybox.get()+"\n")
+        textbox.configure(state="disabled")
+        entrybox.delete(0, tk.END)
+keyboard.add_hotkey('enter',type)
 
+root = tk.Tk()
 root.title="program"
-root.grid_columnconfigure(0, weight=1)  # left side expands
-root.grid_columnconfigure(1, weight=0)  # right side fixed
-textbox=tk.Text(root,font="Ariel")
-textbox.grid(row=1,column=0,padx=10,pady=1,rowspan=10)
+root.geometry("1000x600")
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=0)
+root.update_idletasks()
+win_width = root.winfo_screenwidth()
+win_height = root.winfo_screenheight()
+textbox=tk.Text(root,font="Ariel",state="disabled",width=win_width)
+textbox.grid(row=1,column=0,padx=10,pady=1,rowspan=5)
+entrybox=tk.Entry(root,font="Ariel",width=win_width)
+entrybox.grid(row=23,column=0,padx=10,pady=5)
 label=tk.Label(root,font="Ariel",text="Hello")
 label.grid(row=0,column=1,padx=10,pady=5)
 label2=tk.Label(root,font="Ariel",text="Chat",anchor="center")
@@ -42,9 +58,8 @@ for i in range(20):
     btn = tk.Button(scrollable_frame, text=f"Butt {i+1}", width=18,command=lambda i=i: button_click(i+1))
     btn.bind("<Enter>", lambda e: canvas.bind_all("<MouseWheel>", _on_mousewheel))
     btn.bind("<Leave>", lambda e: canvas.unbind_all("<MouseWheel>"))
-    btn.grid(row=i, column=0, padx=5, pady=2, sticky="ew")
+    btn.grid(column=0, padx=5, pady=2, sticky="ew")
 
 scrollable_frame.grid_columnconfigure(0, weight=1)
-
 
 root.mainloop()
