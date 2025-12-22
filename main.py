df44bfcb5dbb987ac7c514ad21f64af5a3c534ee
Alan-Wein <alonweintraub.w@gmail.com>
2025-12-22 15:17:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ import socket
 import threading
 import time
 import screen
-import tkinter as tk
+import keyboard
+
+from screen import entrybox
+
 idF=-1
 s = socket.socket()
 s.connect(("127.0.0.1", 9999))
+
+def enter_pressed(entrybox,textbox,id):
+    text=entrybox.get()
+    screen.enter_pressed(entrybox,textbox)
+    if text.startswith("/"):
+        text=text[1:]
+        s.send(f"CMD|{id}|{text}".encode())
+
 def login():
 
     email = input("Enter email: ")
@@ -39,16 +50,21 @@ def start(id):
     root=screen.root("hi","1000x600",True)
     textbox=screen.textbox(root)
     textbox.grid(row=1, column=0, padx=10, pady=1, rowspan=5)
+
+
     label=screen.label(root,"Friend List")
     label.grid(row=0, column=1, padx=10, pady=5)
-
     s.send(f"CMD|{id}|list".encode())
     friends = s.recv(2048).decode().split("|")[1][1:-1].split(",")
     screen.scrollbar(root,2,1,friends,textbox)
+    entry=screen.entrybox(root)
+    entry.grid(row=10, column=0, padx=10, pady=1, rowspan=1)
+    keyboard.add_hotkey('enter', lambda :enter_pressed(entry,textbox,id))
+
+    threading.Thread(target=listen, args=(s,textbox), daemon=True).start()
 
 
     root.mainloop()
-    # threading.Thread(target=listen, args=(s,), daemon=True).start()
     # talk(s,id)
 
 def talk(s,id):
@@ -71,36 +87,38 @@ def talk(s,id):
         s.send(f"CMD|{id}|{text}".encode())
 
 
-def listen(s):
-    global idF
+def listen(s,textbox):
+    # global idF
     while True:
         reply = s.recv(2048).decode()
         if reply == "EXIT":
             break
         if reply.startswith("FRIENDS"):
-            print("Friends list:", reply.split("|")[1])
+            text="Friends list:"+ reply.split("|")[1]
 
-        elif reply.startswith("FRIEND_R"):
-            idF=reply.split("|")[1]
-            continue
+        # elif reply.startswith("FRIEND_R"):
+        #     idF=reply.split("|")[1]
+        #     continue
 
         elif reply.startswith("ME"):
-            print("You are:", reply.split("|")[1])
+            text="You are:"+ reply.split("|")[1]
 
         elif reply.startswith("ONLINE"):
-            print("Online list:", reply.split("|")[1])
+            text="Online list:"+ reply.split("|")[1]
 
         elif reply.startswith("ADDED"):
-            print(f"Friend {reply.split("|")[1]} added!")
+            text=f"Friend {reply.split("|")[1]} added!"
 
         elif reply.startswith("DENIED"):
-            print(f"Friend {reply.split("|")[1]} DENIED your request!")
+            text=f"Friend {reply.split("|")[1]} DENIED your request!"
 
         elif reply == "REQUESTED":
-            print("Friend request sent")
+            text="Friend request sent"
 
         elif reply == "INVALID":
-            print("Invalid command")
+            text="Invalid command"
+
+        screen.type(textbox,"Server> "+text+'\n')
 
 
 if __name__=="__main__":
