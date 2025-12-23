a65591a11ec2586b9b0e7939f3df5e7c96bd331c
Alan-Wein <alonweintraub.w@gmail.com>
2025-12-23 12:41:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
+import json
 import socket
 import threading
 import time
 import screen
 import keyboard
 
-from screen import entrybox
 
 idF=-1
 s = socket.socket()
@@ -17,78 +17,105 @@ def enter_pressed(entrybox,textbox,id):
         text=text[1:]
         s.send(f"CMD|{id}|{text}".encode())
 
-def login():
 
-    email = input("Enter email: ")
-    name = input("Enter name: ")
-    password = input("Enter password: ")
+def login(root,entryE,entryU,entryP):
+
+    email = entryE.get()
+    name = entryU.get()
+    password = entryP.get()
+
     if email == "e" and name == "e" and password == "e":
         s.send("EXIT".encode())
+        root.destroy()
         return
     if email == "clear" and name == "e" and password == "e":
         s.send("CLEAR".encode())
-        login()
+        entryE.delete(0,"end")
+        entryU.delete(0,"end")
+        entryP.delete(0,"end")
+
         return
     else:
         s.send(f"LOGIN|{email}|{name}|{password}".encode())
     response = s.recv(2048).decode()
 
     if response == "NO":
-        print("Name or password incorrect")
-        login()
+        screen.popup("Name or password incorrect. Try again")
+        entryE.delete(0,"end")
+        entryU.delete(0,"end")
+        entryP.delete(0,"end")
+        return
 
     elif response.startswith("NEW"):
         id = response.split("|")[1]
-        print("Hello for the first time!")
+        root.destroy()
         start(id)
+
     elif response.startswith("OK"):
         id = response.split("|")[1]
-        print("Welcome back!")
+        root.destroy()
         start(id)
+
+
+def begin():
+    root = screen.root("Login","400x320",False)
+
+    labelE=screen.label(root, text="Email:").pack(anchor="w", padx=20, pady=(15, 0))
+    entryE=screen.entrybox(root)
+    entryE.pack(fill="x", padx=20)
+
+
+    labelU=screen.label(root, text="Username:").pack(anchor="w", padx=20, pady=(10, 0))
+    entryU=screen.entrybox(root)
+    entryU.pack(fill="x", padx=20)
+
+    labelP=screen.label(root, text="Password:").pack(anchor="w", padx=20, pady=(10, 0))
+    entryP=screen.entrybox(root,True)
+    entryP.pack(fill="x", padx=20)
+
+    button=screen.button(root, text="Log In",comand=lambda :login(root,entryE,entryU,entryP))
+    button.pack(pady=20)
+
+    root.mainloop()
+
 
 def start(id):
     root=screen.root("hi","1000x600",True)
     textbox=screen.textbox(root)
     textbox.grid(row=1, column=0, padx=10, pady=1, rowspan=5)
 
+    entryADD=screen.entrybox(root)
+    entryADD.grid(row=2,column=1,padx=10, pady=1)
+    buttonADD=screen.button(root,"Add Friend(input id)",comand=lambda :addFriend(id,entryADD))
+    buttonADD.grid(row=1,column=1,padx=10, pady=1)
 
-    label=screen.label(root,"Friend List")
-    label.grid(row=0, column=1, padx=10, pady=5)
+    labelF=screen.label(root,"Friend List")
+    labelF.grid(row=3, column=1, padx=10, pady=5)
     s.send(f"CMD|{id}|list".encode())
-    friends = s.recv(2048).decode().split("|")[1][1:-1].split(",")
-    screen.scrollbar(root,2,1,friends,textbox)
+    friends = json.loads(s.recv(2048).decode().split("|")[1])
+    print(friends)
+    screen.scrollbar(root,4,1,friends,textbox)
     entry=screen.entrybox(root)
+    entry.configure(width=root.winfo_screenwidth())
     entry.grid(row=10, column=0, padx=10, pady=1, rowspan=1)
     keyboard.add_hotkey('enter', lambda :enter_pressed(entry,textbox,id))
 
-    threading.Thread(target=listen, args=(s,textbox), daemon=True).start()
+
+    s.send(f"OFFLINE|{id}".encode())
+    print("sent")
+    threading.Thread(target=listen, args=(s,textbox,id), daemon=True).start()
 
 
     root.mainloop()
-    # talk(s,id)
-
-def talk(s,id):
-    global idF
-    # while True:
-    time.sleep(0.1)
-    if idF!=-1:
-        answer = input(f"id - {idF} requested friendship, answer Y/N: ")
-        while answer != "Y" and answer != "N":
-            answer = input("Wrong answer, try again: ")
-        s.send(f"FRIEND_A|{id}|{idF}|{answer}".encode())
-        idF=-1
-    else:
-        text = input("> ")
-
-        if text == "exit":
-            s.send("EXIT".encode())
-            print("Goodbye")
-            # break
-        s.send(f"CMD|{id}|{text}".encode())
+def addFriend(id,entryADD):
+    print(entryADD.get())
+    s.send(f"CMD|{id}|add {entryADD.get()}".encode())
+    entryADD.delete(0,"end")
 
 
-def listen(s,textbox):
-    # global idF
+
+
+def listen(s,textbox,id):
     while True:
         reply = s.recv(2048).decode()
         if reply == "EXIT":
@@ -96,9 +123,14 @@ def listen(s,textbox):
         if reply.startswith("FRIENDS"):
             text="Friends list:"+ reply.split("|")[1]
 
-        # elif reply.startswith("FRIEND_R"):
-        #     idF=reply.split("|")[1]
-        #     continue
+        elif reply.startswith("FRIEND_R"):
+            print("works?")
+            idf=reply.split("|")[1]
+            answer=screen.question(f"Do you want to be friends with {idf}?")
+            if answer: text="Y"
+            else: text="N"
+            s.send(f"FRIEND_A|{id}|{idf}|{answer}".encode())
+
 
         elif reply.startswith("ME"):
             text="You are:"+ reply.split("|")[1]
@@ -107,19 +139,19 @@ def listen(s,textbox):
             text="Online list:"+ reply.split("|")[1]
 
         elif reply.startswith("ADDED"):
-            text=f"Friend {reply.split("|")[1]} added!"
+            screen.popup(f"Friend {reply.split("|")[1]} added!")
+            continue
 
         elif reply.startswith("DENIED"):
-            text=f"Friend {reply.split("|")[1]} DENIED your request!"
-
-        elif reply == "REQUESTED":
-            text="Friend request sent"
+            screen.popup(f"Friend {reply.split("|")[1]} DENIED your request!")
+            continue
 
         elif reply == "INVALID":
-            text="Invalid command"
+            screen.popup("INVALIIIIID")
+            continue
 
         screen.type(textbox,"Server> "+text+'\n')
 
 
 if __name__=="__main__":
-    login()
+    begin()
